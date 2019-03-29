13116a215348b1a64548d0fdb50925e6aa1a8430
Frank Vinci <fv42wid@yahoo.com>
2019-03-29 16:20:06 -0400

    database set up, vue running, create card from vue next

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,35 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
+  <div id="app" class="row">
+    <div v-for="(list, index) in original_lists" class="col-3">
+      <h6>{{ list.name }}</h6>
+      <hr />
+
+      <div v-for="(card, index) in list.cards" class="card card-body mb-3">
+        {{ card.name }}
+      </div>
+
+      <div class="card card-body">
+        <textarea v-model="messages[list.id]" class="form-control"></textarea>
+        <button v-on:click="submitMessages(list.id)" class="btn btn-primary">Save</button>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  props: ["original_lists"],
+  data: function() {
     return {
-      message: "Hello Vue!"
+      messages: {
+
+      }
+    }
+  },
+  methods: {
+    submitMessages: function(list_id) {
+      console.log(this.messages[list_id]);
     }
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,20 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+//console.log('Hello World from Webpacker')
+
+import Vue from "vue/dist/vue.esm"
+import App from "../app.vue"
+document.addEventListener("turbolinks:load", function() {
+    var element = document.querySelector("#boards")
+    if(element != undefined) {
+        const app = new Vue({
+            el: element,
+            data: {
+                lists: JSON.parse(element.dataset.lists)
+            },
+            template: "<App :original_lists='lists' />",
+            components: { App }
+        })
+    }
+})
